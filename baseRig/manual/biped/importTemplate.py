4b1a76e81aac1d53e42cdce8c3c3eda14b7fe6f8
mappp7 <56536931+mappp7@users.noreply.github.com>
2020-02-25 13:52:04 +0900

    linix>window
    path transfer

--- baseRig/manual/biped/importTemplate.py
+++ baseRig/manual/biped/importTemplate.py
@@ -8,19 +8,19 @@ import maya.cmds as cmds
 import maya.mel as mel
 from util.defaultGroupNode import *
 
-class importTemplate(): 
+class importTemplate():
     '''
     2족 템플릿 조인트를 import 하는 클래스
-    
+
     '''
     def __init__(self):
         z=defaultGroupNode()
         z.createGroupNode()
         return
-        
+
     def simpleBiped(self):
-        
-        cmds.file ("/dexter/Cache_DATA/CRT/RiggingRnD/baseRig/manual/biped/template/simpleBiped.mb",i=True , loadReferenceDepth= "all")
+
+        cmds.file ("C:\tools\tools\manual\biped\template\simpleBiped.mb",i=True , loadReferenceDepth= "all")
 
         cmds.parent ('C_template_hip_JNT','L_template_shoulder_JNT','R_template_shoulder_JNT',
         'R_template_upArm_JNT','L_template_upArm_JNT','C_template_neck_JNT','L_template_leg_JNT',
